15fd37b77bad361cf9acb7514d3d3c24a9073098
RonanCraft <SuperRonanCraft@gmail.com>
2024-01-17 16:07:29 -0500

    claim world perms

--- src/main/java/me/RonanCraft/BetterClaims/resources/Permissions.java
+++ src/main/java/me/RonanCraft/BetterClaims/resources/Permissions.java
@@ -1,7 +1,9 @@
 package me.RonanCraft.BetterClaims.resources;
 
 import me.RonanCraft.BetterClaims.resources.dependencies.DepPermissions;
+import org.bukkit.World;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Permissions {
     private final DepPermissions depPerms = new DepPermissions();
@@ -12,5 +14,13 @@ public class Permissions {
 
     public boolean checkPerm(String str, CommandSender sendi) {
         return depPerms.hasPerm(str, sendi);
+    }
+
+    public static boolean canCreateClaimIn(Player player, World world) {
+        return PermissionNodes.CLAIM_WORLD.check(player, world);
+    }
+
+    public static boolean getMaxClaimSize(Player player) {
+        return false;
     }
 }
